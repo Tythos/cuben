06dbe221e2bb521e863d995ab14f62f4675a586f
Tythos <code@tythos.net>
2016-07-12 23:34:26 -0700

    Aligning build artifact tag format with repository tag (will eventually be automated)

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-set ver=0.0.1
+set ver=v0.0.1
 set name=cuben
 cl /EHsc /nologo main.cpp src/base.cpp src/fund.cpp src/equations.cpp src/systems.cpp src/interp.cpp /Iinc/ /I%appdata%/EPiC/inc /Febin/%name%.exe
 lib base.obj fund.obj equations.obj systems.obj interp.obj -OUT:lib/%name%.lib
