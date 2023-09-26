fd6c99c539911f2d4b096e520344ec5bd56e6246
Brian Kirkpatrick <code@tythos.net>
2023-09-25 17:34:12 -0700

    piecemeal refactoring

---
+++ constants.cpp
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+
+#include "constants.hpp"
+
+const float cuben::constants::iterTol = 1e-8;
+const float cuben::constants::zeroTol = 1e-8;
+const float cuben::constants::adaptiveTol = 1e-4;
+const float cuben::constants::relDiffEqTol = 1e-6;
+const float cuben::constants::bvpZeroTol = 1e-6;
+const int cuben::constants::iterLimit = 1e4;
---
+++ constants.hpp
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+#ifndef CUBEN_CONSTANTS
+#define CUBEN_CONSTANTS
+
+namespace cuben {
+    namespace constants {
+        extern const float iterTol;
+        extern const float zeroTol;
+        extern const float adaptiveTol;
+        extern const float relDiffEqTol;
+        extern const float bvpZeroTol;
+        extern const int iterLimit;
+    }
+}
+
+#endif
